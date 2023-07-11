c0c0f5d4efd26bbfc95a9098cf32334f66915296
hugo3465 <huguito.ricardo@hotmail.com>
2023-07-11 14:53:48 +0100

    Correção do erro do css na área de administração

--- Administrador/Base de dados/registos.php
+++ Administrador/Base de dados/registos.php
@@ -15,9 +15,9 @@
         <link rel="stylesheet" href="css/w3.css"> <!-- a biblioteca w3.css serve apenas para os panels -->
 
         <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery-3.3.1.slim.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         
         <!-- Icons Google -->
         <link rel="stylesheet" href="css/icon.css">
